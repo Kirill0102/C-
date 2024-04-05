b8571d287ab7926e9ed2bd57b9c0116e71b15ba3
Kirill Lykov <kirillykov07@gmail.com>
2024-04-05 21:40:21 +0300

    first commit Programs.cs Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Привет");
+
+int n1 = 4;
+double n2 = 3.4;
+bool n3 = true;
+
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
