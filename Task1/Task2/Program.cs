7664adcbe2a6fd944e0b6b1c063c1e6123e0d9b6
Kirill Lykov <kirillykov07@gmail.com>
2024-04-05 22:18:01 +0300

    add commit C#:Task2

---
+++ Task1/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// int n1 = 100;
+// Console.WriteLine(n1);
+
+int n2 = 5;
+int n3 = 10;
+int sum = n2 + n3 + n2;
+Console.WriteLine(sum);
+
+double n4 = 5.5;
+double sum2 = n4 + n3;
+char d = '!';
+Console.WriteLine(d);
